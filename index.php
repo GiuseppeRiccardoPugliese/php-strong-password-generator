b55b0c55e8c38f7728f5665aa14ffb143d6d8a41
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-01-24 16:01:25 +0100

    Milestone 2: spostare tutta la logica in un altro file

--- index.php
+++ index.php
@@ -15,38 +15,9 @@
         <button type="submit">Genera Password</button>
     </form>
 
+    <!-- MILESTONE 2 -->
     <?php
-    $lungpsw = $_GET['lunghezza'];
-
-    function generaPasswordCasuale($lungpsw)
-    {
-        $caratteriPermessi = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_';
-        $lunghezzaCaratteri = strlen($caratteriPermessi);
-        $password = '';
-
-        //Ciclo sulla lunghezza inserita dall'utente
-        for ($i = 0; $i < $lungpsw; $i++) {
-            $indiceCasuale = rand(0, $lunghezzaCaratteri - 1);
-            $password .= $caratteriPermessi[$indiceCasuale];
-        }
-
-        return $password;
-    }
-    // Verifico se la lunghezza e' settata, allora:
-    if (isset($_GET['lunghezza'])) {
-        $lunghezzaPassword = intval($_GET['lunghezza']); //Mi faccio ritornare un'intero
-
-        // Condizione per farsi' che la lunghezza sia valida (positiva e non troppo grande(128 in base ai BIT))
-        if ($lunghezzaPassword > 0 && $lunghezzaPassword <= 128) {
-            // Genera la password casuale
-            $passwordCasuale = generaPasswordCasuale($lunghezzaPassword);
-            echo "La tua password casuale è: $passwordCasuale";
-        } else {
-            echo "La lunghezza della password deve essere un numero positivo non troppo grande.";
-        }
-    } else {
-        echo "Devi fornire la lunghezza della password.";
-    }
+    require_once __DIR__ . '/functions.php';
     ?>
 </body>
 
